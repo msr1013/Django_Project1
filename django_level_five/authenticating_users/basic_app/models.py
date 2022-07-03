a795e3887949abf583c6df3135591914a3926089
Manish Rawal <manish.rawal@convergetree.com>
2022-07-03 17:33:08 +0545

    Class Based View using from django.views.generic import View

--- django_level_five/authenticating_users/basic_app/models.py
+++ django_level_five/authenticating_users/basic_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
 
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
 
     # additional classes
 
